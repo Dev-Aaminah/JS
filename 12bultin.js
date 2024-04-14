aa68329e6158efccd137baee6467656596a86ff1
Dev-Aaminah <aminakanwal12345@gmail.com>
2024-04-14 21:17:02 +0500

    Learnt about bultin functions in javascript.

---
+++ 12bultin.js
@@ -0,0 +1,18 @@
+// String Bultins
+
+let string = "Hello World!";
+// string = string.concat("This is Aaminah");
+// string = string.charAt(8);
+// string = string.padStart(3);
+string = string.toUpperCase();
+console.log(string);
+
+
+// Number Bultins
+
+let number = 2.99;
+// num = num.toExponential(3);
+// number = Math.round(number);
+// number = Math.SQRT2;
+// number = Math.random();
+console.log(number);
